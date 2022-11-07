77971e6703d0c188c238f3f14543b286bbc903fe
Andria <ostro2al@cmich.edu>
2022-11-07 11:59:07 +1000

    AO update GAMs

--- scripts/field stats GAMs.R
+++ scripts/field stats GAMs.R
@@ -134,3 +134,260 @@ AIC(m1, m2)
 #block interaction seems to be very important!
 #But you may want to plot results and see if m2 has
 # a different interpretation for treatment
+
+#plots 
+#Treatment effects
+predout <- visreg(m2, xvar = "Week", 
+                  by = "Treatment",
+                  scale = "response",
+                  plot = FALSE,
+                  alpha = 0.05) 
+
+g1 <- ggplot(predout$fit) +
+  aes(x = Week, y = visregFit, color = Treatment,
+      group = Treatment, fill = Treatment)+ 
+  geom_line() + 
+  geom_ribbon(aes(ymin = visregLwr, ymax = visregUpr),
+              alpha = 0.2, color = NA)
+
+#Block effects
+predout_block <- visreg(m2, xvar = "Week", 
+                        by = "Block",
+                        scale = "response",
+                        plot = FALSE,
+                        alpha = 0.05) # can plot directly with TRUE here
+
+g2 <- ggplot(predout_block$fit) +
+  aes(x = Week, y = visregFit, color = Block,
+      group = Block, fill = Block)+ 
+  geom_line() + 
+  geom_ribbon(aes(ymin = visregLwr, ymax = visregUpr),
+              alpha = 0.2, color = NA)
+
+g1 + g2 + plot_annotation(tag_levels = 'A')
+
+#Similar model output from first model 
+#same trends in treatment effects - all treatments reduced shoot density
+#static slowest reduction
+#out phase greater reduction, in phase greatest reduction
+#effect of block still
+#model 1 indicates there is variability of block effect over time,
+#with greater reduction in shoots in blocks 2 and 4 starting at week 4
+
+
+
+#
+#AO additional response variables - Nov 11, 2022
+#leaf surface area
+m1 <- gam(Leaf_surface_area ~ s(Week, by = Block, k = 3) +
+            s(Week, by = Treatment, k = 3) + 
+            s(Block, bs = "re") + 
+            offset(Day0_surface_area),
+          data = dat)
+gam.check(m1)
+
+m2 <- gam(Leaf_surface_area ~ s(Week, by = Block, k = 3) +
+            Treatment + 
+            s(Block, bs = "re") +
+            offset(Day0_surface_area),
+          data = dat)
+
+gam.check(m2)
+
+m3 <- gam(Leaf_surface_area ~ s(Week, by = Block, k = 3) +
+            Treatment + 
+            s(Week, by = Treatment, k = 3) + 
+            s(Block, bs = "re") + 
+            offset(Day0_surface_area),
+          data = dat)
+
+gam.check(m3)
+plot(m3)
+summary(m3)
+
+#plots
+#Treatment effects
+predout <- visreg(m3, xvar = "Week", 
+                  by = "Treatment",
+                  scale = "response",
+                  plot = FALSE,
+                  alpha = 0.05)  
+
+g1 <- ggplot(predout$fit) +
+  aes(x = Week, y = visregFit, color = Treatment,
+      group = Treatment, fill = Treatment)+ 
+  geom_line() + 
+  geom_ribbon(aes(ymin = visregLwr, ymax = visregUpr),
+              alpha = 0.2, color = NA)
+g1
+
+#Block effects
+predout_block <- visreg(m3, xvar = "Week", 
+                        by = "Block",
+                        scale = "response",
+                        plot = FALSE,
+                        alpha = 0.05) # can plot directly with TRUE here
+
+g2 <- ggplot(predout_block$fit) +
+  aes(x = Week, y = visregFit, color = Block,
+      group = Block, fill = Block)+ 
+  geom_line() + 
+  geom_ribbon(aes(ymin = visregLwr, ymax = visregUpr),
+              alpha = 0.2, color = NA)
+g2
+
+g1 + g2 + plot_annotation(tag_levels = 'A')
+
+#higher variability/larger CIs for LSA
+#R-sq value is low for all three models
+#significant block effect - greater reduction across weeks in block 2
+#gradual reduction in static treatment
+#greater reduction in in and out phase treatments, although overlap CIs with static treatment
+#model summary indicates significant effect of both in and out phase treatments
+
+#can we log transform LSA?
+dat$lnarea <- log(dat$Leaf_surface_area)
+dat$lnDay0area <- log(dat$Day0_surface_area)
+
+m4 <- gam(lnarea ~ s(Week, by = Block, k = 3) + Treatment + 
+            s(Week, by = Treatment, k = 3) + s(Block, bs = "re") +
+            offset(lnDay0area), data = dat)
+
+gam.check(m4)
+plot(m4)
+summary(m4)
+#seems to be better fit..
+
+#plots
+#Treatment effects
+predout <- visreg(m4, xvar = "Week", 
+                  by = "Treatment",
+                  scale = "response",
+                  plot = FALSE,
+                  alpha = 0.05)  
+
+g1 <- ggplot(predout$fit) +
+  aes(x = Week, y = visregFit, color = Treatment,
+      group = Treatment, fill = Treatment)+ 
+  geom_line() + 
+  geom_ribbon(aes(ymin = visregLwr, ymax = visregUpr),
+              alpha = 0.2, color = NA)
+g1
+
+#Block effects
+predout_block <- visreg(m4, xvar = "Week", 
+                        by = "Block",
+                        scale = "response",
+                        plot = FALSE,
+                        alpha = 0.05) # can plot directly with TRUE here
+
+g2 <- ggplot(predout_block$fit) +
+  aes(x = Week, y = visregFit, color = Block,
+      group = Block, fill = Block)+ 
+  geom_line() + 
+  geom_ribbon(aes(ymin = visregLwr, ymax = visregUpr),
+              alpha = 0.2, color = NA)
+g2
+
+g1 + g2 + plot_annotation(tag_levels = 'A')
+#significant effect of block 4 over time
+
+
+#crustacean abundance
+m1 <- gam(Total_crustacean ~ s(Week, by = Block, k = 3) +
+            s(Week, by = Treatment, k = 3) + 
+            s(Block, bs = "re") + 
+            offset(Day0_crustacean),
+          data = dat)
+gam.check(m1)
+
+m2 <- gam(Total_crustacean ~ s(Week, by = Block, k = 3) +
+            Treatment + 
+            s(Week, by = Treatment, k = 3) + 
+            s(Block, bs = "re") + 
+            offset(Day0_crustacean),
+          data = dat)
+gam.check(m2)
+
+#poisson - this is count data..
+m3 <- gam(Total_crustacean ~ s(Week, by = Block, k = 3) + Treatment + 
+            s(Week, by = Treatment, k = 3) + 
+            s(Block, bs = "re") + offset(log(Day0_crustacean + 0.01)),
+          family = "poisson",
+          data = dat)
+gam.check(m3)
+plot(m3)
+summary(m3)
+
+m4 <- gam(Total_crustacean ~ s(Week, by = Block, k = 3) + Treatment + 
+            s(Block, bs = "re") + offset(log(Day0_crustacean + 0.01)),
+          family = "poisson",
+          data = dat)
+
+gam.check(m4)
+
+#log transform data?
+dat$lncrustacean <- log(dat$Total_crustacean + 0.01)
+dat$lnDay0crustacean <- log(dat$Day0_crustacean + 0.01)
+
+m5 <- gam(lncrustacean ~ s(Week, by = Block, k = 3) +
+            Treatment + 
+            s(Week, by = Treatment, k = 3) + 
+            s(Block, bs = "re") + 
+            offset(lnDay0crustacean),
+          data = dat)
+gam.check(m5)
+
+#square root transform?
+dat$SRcrustacean <- sqrt(dat$Total_crustacean)
+dat$SRDay0crustacean <- sqrt(dat$Day0_crustacean)
+
+m6 <- gam(SRcrustacean ~ s(Week, by = Block, k = 3) +
+            Treatment + 
+            s(Week, by = Treatment, k = 3) + 
+            s(Block, bs = "re") + 
+            offset(SRDay0crustacean),
+          data = dat)
+gam.check(m6)
+plot(m6)
+summary(m6)
+
+#plots
+#Treatment effects
+predout <- visreg(m3, xvar = "Week", 
+                  by = "Treatment",
+                  scale = "response",
+                  plot = FALSE,
+                  alpha = 0.05)  
+
+g1 <- ggplot(predout$fit) +
+  aes(x = Week, y = visregFit, color = Treatment,
+      group = Treatment, fill = Treatment)+ 
+  geom_line() + 
+  geom_ribbon(aes(ymin = visregLwr, ymax = visregUpr),
+              alpha = 0.2, color = NA)
+g1
+
+#Block effects
+predout_block <- visreg(m3, xvar = "Week", 
+                        by = "Block",
+                        scale = "response",
+                        plot = FALSE,
+                        alpha = 0.05) # can plot directly with TRUE here
+
+g2 <- ggplot(predout_block$fit) +
+  aes(x = Week, y = visregFit, color = Block,
+      group = Block, fill = Block)+ 
+  geom_line() + 
+  geom_ribbon(aes(ymin = visregLwr, ymax = visregUpr),
+              alpha = 0.2, color = NA)
+g2
+
+g1 + g2 + plot_annotation(tag_levels = 'A')
+
+#model 3 seems best fit
+#low R-sq values across all models
+#significant effect of block over time - high variability across blocks and time
+#effect across all treatments
+#static treatment reduces abundance
+#in and out phase treatments indicate greater reduction than static
