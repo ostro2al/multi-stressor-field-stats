1b65a40444279c781beae825e900248e4ad831c8
Andria <ostro2al@cmich.edu>
2023-01-25 15:42:28 +1000

    AO updated pSEMs

--- scripts/Working SEM stats_AO.R
+++ scripts/Working SEM stats_AO.R
@@ -59,85 +59,8 @@ summary(m3_pSEM_randomList_CB, .progressBar = F)
 #####--------------------------- ##########
 
 ### psem 
-#with random block effect
-m1_pSEM_randomList = psem(
-  lme(Shoot_density ~ Stressor_app + Week, random = ~ 1 | Block, data = dat),
-  lme(Avg_LSA ~ Stressor_app  + Week, random = ~ 1 | Block, data = dat),
-  lme(Crustacean_abundance ~ Stressor_app + Week + Shoot_density + Avg_LSA, random = ~ 1 | Block, data = dat)
-)
-
-summary(m1_pSEM_randomList, .progressBar = F)
-
-
-m2_pSEM_randomList = psem(
-  lme(Shoot_density ~ Stressor_app + Week + Avg_LSA, random = ~ 1 | Block, data = dat),
-  lme(Avg_LSA ~ Stressor_app  + Week, random = ~ 1 | Block, data = dat),
-  lme(Crustacean_abundance ~ Stressor_app + Week + Shoot_density + Avg_LSA, random = ~ 1 | Block, data = dat)
-)
-
-summary(m2_pSEM_randomList, .progressBar = F)
-
-
-#without random block effect
-m1_pSEM = psem(
-  lm(Shoot_density ~ Stressor_app + Week, data = dat),
-  lm(Avg_LSA ~ Stressor_app + Week, data = dat),
-  lm(Crustacean_abundance ~ Stressor_app + Week + Shoot_density + Avg_LSA, data = dat)
-)
-
-summary(m1_pSEM, .progressBar = F)
-
-
-m2_pSEM = psem(
-  lm(Shoot_density ~ Stressor_app + Week + Avg_LSA, data = dat),
-  lm(Avg_LSA ~ Stressor_app  + Week, data = dat),
-  lm(Crustacean_abundance ~ Stressor_app + Week + Shoot_density + Avg_LSA, data = dat)
-)
-
-summary(m2_pSEM, .progressBar = F)
-
-plot(m2_pSEM)
-
-dSep(m2_pSEM)
-coefs(m2_pSEM, standardize = "none")
-residuals(m2_pSEM)
-plot(residuals(m2_pSEM))
-
-
-plot(m2_pSEM, return = FALSE,
-     node_attrs = data.frame(shape = "rectangle", color = "black", fillcolor = "white"),
-     edge_attrs = data.frame(style = "solid", color = "black"),
-     ns_dashed = T,
-     alpha = 0.05,
-     show = "std",
-     digits = 3,
-     add_edge_label_spaces = TRUE)
-
-#model 2 has lowest AIC and BIC in psem - lowest without random block effect
-
-#transform crustacean abundance
-dat$lncrust <- log(dat$Crustacean_abundance + 0.001)
-dat$SRcrust <- sqrt(dat$Crustacean_abundance)
-
-m3_pSEM = psem(
-  lm(Shoot_density ~ Stressor_app + Week + Avg_LSA, data = dat),
-  lm(Avg_LSA ~ Stressor_app  + Week, data = dat),
-  lm(lncrust ~ Stressor_app + Week + Shoot_density + Avg_LSA, data = dat)
-)
-
-summary(m3_pSEM, .progressBar = F)
-
-plot(m3_pSEM)
-
-dSep(m3_pSEM)
-fisherC(m3_pSEM)
-coefs(m3_pSEM, standardize = "none")
-residuals(m3_pSEM)
-
-
-#
-#Latest code revisions (13.01.2023) - AO
-#revised psem with random block effect and interaction between treatment and week
+#Latest code revisions (25.01.2023) - AO
+#revised psem with random block effect
 
 #load csv with data stacked to include day 0 data
 dat <- read.csv("DataStacked_Control.csv")
@@ -146,119 +69,138 @@ dat$Stressor_app <- as.numeric(dat$Stressor_app)
 dat$Week <- as.numeric(dat$Week)
 str(dat)
 
-dat <- dat %>% na.omit()
+#try different pathway hypotheses
+#should we also look at effects of LSA on density instead of correlaton between the variables??
+#week * treatment interaction, mediation of crustaceans
+m1 = psem(
+  lme(Shoot_density ~ Stressor_app * Week, random = ~ 1 | Block, data = dat),
+  lme(Avg_LSA ~ Stressor_app * Week, random = ~ 1 | Block, data = dat),
+  lme(Crustacean_abundance ~ Week + Shoot_density + Avg_LSA, random = ~ 1 | Block, data = dat),
+  Avg_LSA %~~% Shoot_density
+)
+
+summary(m1, .progressBar = F)
+
+#no interaction, mediation of crustaceans (no direct treatment effect)
+m2 = psem(
+  lme(Shoot_density ~ Stressor_app + Week, random = ~ 1 | Block, data = dat),
+  lme(Avg_LSA ~ Stressor_app + Week, random = ~ 1 | Block, data = dat),
+  lme(Crustacean_abundance ~ Shoot_density + Avg_LSA, random = ~ 1 | Block, data = dat),
+  Avg_LSA %~~% Shoot_density
+)
+
+summary(m2, .progressBar = F)
+#doesn't fit, suggests week effect needed for crusts
+
+#add effect of week on crustaceans
+m3 = psem(
+  lme(Shoot_density ~ Stressor_app + Week, random = ~ 1 | Block, data = dat),
+  lme(Avg_LSA ~ Stressor_app + Week, random = ~ 1 | Block, data = dat),
+  lme(Crustacean_abundance ~ Week + Shoot_density + Avg_LSA, random = ~ 1 | Block, data = dat),
+  Avg_LSA %~~% Shoot_density
+)
+
+summary(m3, .progressBar = F)
+anova(m3)
+plot(m3)
+#check residuals 
+plot(resid(lme(Shoot_density ~ Stressor_app + Week, random = ~ 1 | Block, data = dat)))
+plot(resid(lme(Avg_LSA ~ Stressor_app + Week, random = ~ 1 | Block, data = dat)))
+plot(resid(lme(Crustacean_abundance ~ Week + Shoot_density + Avg_LSA, random = ~ 1 | Block, data = dat)))
+
+#try week and stressor app effect on crustaceans
+m4 = psem(
+  lme(Shoot_density ~ Stressor_app + Week, random = ~ 1 | Block, data = dat),
+  lme(Avg_LSA ~ Stressor_app  + Week, random = ~ 1 | Block, data = dat),
+  lme(Crustacean_abundance ~ Stressor_app + Week + Shoot_density + Avg_LSA, random = ~ 1 | Block, data = dat),
+  Avg_LSA %~~% Shoot_density
+)
+
+summary(m4, .progressBar = F)
+#same results as m3, no sig effect of treatment on crustaceans
+
+#
+#compare all models that fit: m1, m3, m4 
+anova(m1, m3, m4)
+#Chi-square Difference Test
+#AIC    BIC Fisher.C  Fisher.C.Diff  DF.diff      P.value    
+#1    39.106 98.461    1.106                               
+#vs 2 33.106 83.089    1.106             0       0       1 
+#vs 3 34.000 87.107    0.000         1.106       2  0.5752 
+
+AIC(m1, m3)
+#df    AIC
+#x 18 37.106
+#y 16 33.106
+
+#m1 summary
+#AIC      BIC
+#37.106   93.337
+#Fisher's C = 1.106 with P-value = 0.575 and on 2 degrees of freedom
+
+#m3
+#AIC      BIC
+#33.106   83.089
+#Fisher's C = 1.106 with P-value = 0.575 and on 2 degrees of freedom
+
+#m4
+#AIC      BIC
+#34.000   87.107
+#Fisher's C = 0 with P-value = 1 and on 0 degrees of freedom
+
+
+#
+##try running pSEM with GAMs 
+str(dat)
+dat$Stressor_app <- as.numeric(dat$Stressor_app)
+dat$Week <- as.numeric(dat$Week)
+dat$Block <- as.numeric(dat$Block)
+str(dat)
 dat
 
-#new method to construct models
-##break down individual regressions (of hypothesised paths of interest) to explore individual
-#and interactive effects, then use AIC to select best regression to input into SEM
+sort(dat$Week)
 
-#shoot density
-m1 <- lmer(Shoot_density ~ Stressor_app + Week + Avg_LSA + (1 | Block), data = dat)
-m2 <- lmer(Shoot_density ~ Stressor_app * Week + Avg_LSA + (1 | Block), data = dat)
-m3 <- lmer(Shoot_density ~ Stressor_app * Week * Avg_LSA + (1 | Block), data = dat)
-m4 <- lmer(Shoot_density ~ Stressor_app + Week * Avg_LSA + (1 | Block), data = dat)
-
-#m2 lowest AIC, m3 highest df
-AIC(m1, m2, m3, m4)
-#   df      AIC
-#m1  6 1654.328
-#m2  7 1646.950
-#m3 10 1648.833
-#m4  7 1658.982
-
-plot(m2)
-qqnorm(resid(m2))
-qqline(resid(m2))
-
-
-#average leaf surface area
-m1 <- lmer(Avg_LSA ~ Stressor_app * Week + (1 | Block), data = dat)
-m2 <- lmer(Avg_LSA ~ Stressor_app + Week + (1 | Block), data = dat)
-
-#m2 lower AIC, m1 higher df
-AIC(m1, m2)
-#   df      AIC
-#m1  6 1262.265
-#m2  5 1261.839
-
-plot(m2)
-qqnorm(resid(m2))
-qqline(resid(m2))
-
-
-#crustacean abundance
-m1 <- lmer(Crustacean_abundance ~ Week + Shoot_density + Avg_LSA + (1 | Block), data = dat)
-m2 <- lmer(Crustacean_abundance ~ Week * Shoot_density + Avg_LSA + (1 | Block), data = dat)
-
-#m1 lowest AIC, m2 higher df
-AIC(m1, m2)
-#df      AIC
-#m1  6 1098.990
-#m2  7 1108.985
-
-plot(m2)
-qqnorm(resid(m2))
-qqline(resid(m2)) #not good fit
-
-#log transform crustacean abundance
-dat$lncrust <- log(dat$Crustacean_abundance + 0.001)
-#square root transform crustacean abundance - seems best fit
-dat$SRcrust <- sqrt(dat$Crustacean_abundance)
-
-m1 <- lmer(SRcrust ~ Week + Shoot_density + Avg_LSA + (1 | Block), data = dat)
-m2 <- lmer(SRcrust ~ Week * Shoot_density + Avg_LSA + (1 | Block), data = dat)
-
-#m1 has lowest AIC, m2 highest df
-AIC(m1, m2)
-#   df      AIC
-#m1  6 541.2747
-#m2  7 554.6181
-
-plot(m1)
-qqnorm(resid(m1))
-qqline(resid(m1))
-
-m1 <- lmer(lncrust ~ Week + Shoot_density + Avg_LSA + (1 | Block), data = dat)
-m2 <- lmer(lncrust ~ Week * Shoot_density + Avg_LSA + (1 | Block), data = dat)
-
-#m1 lowest AIC, m2 highest df
-AIC(m1, m2)
-#   df      AIC
-#m1  6 890.2207
-#m2  7 902.2558
-
-
-##
-#now try SEM with regressions selected based on lowest AIC - second is best (using SQRT transform crustacean variable)
-mnew_pSEM_randomList = psem(
-  lme(Shoot_density ~ Stressor_app * Week + Avg_LSA, random = ~ 1 | Block, data = dat),
-  lme(Avg_LSA ~ Stressor_app + Week, random = ~ 1 | Block, data = dat),
-  lme(Crustacean_abundance ~ Week + Shoot_density + Avg_LSA, random = ~ 1 | Block, data = dat)
+m1_gam = psem(
+  gam(Shoot_density ~ s(Week, by = Block, k = 4) +
+        Stressor_app + s(Week, by = Stressor_app, k = 4) + 
+        s(Block, bs = "re"), family = "poisson", data = dat),
+  gam(Avg_LSA ~ s(Week, by = Block, k = 4) +
+        Stressor_app + s(Week, by = Stressor_app, k = 4) + 
+        s(Block, bs = "re"), data = dat),
+  gam(Crustacean_abundance ~ s(Week, by = Block, k = 4) + Stressor_app + Avg_LSA + 
+        Shoot_density + s(Week, by = Stressor_app, k = 4) + s(Block, bs = "re"), family = nb(), data = dat)
 )
+#issues getting the psem to run
 
-summary(mnew_pSEM_randomList, .progressBar = F)
+nrow(dat)
+length(dat[,1])
 
-plot(mnew_pSEM_randomList)
+length(unique(dat[,1]))==length(dat[,1])
 
+dups <- which(duplicated(dat))
+length(dups)
+unique(dat[dups])
 
-mnew2_pSEM_randomList = psem(
-  lme(Shoot_density ~ Stressor_app * Week + Avg_LSA, random = ~ 1 | Block, data = dat),
-  lme(Avg_LSA ~ Stressor_app + Week, random = ~ 1 | Block, data = dat),
-  lme(SRcrust ~ Week + Shoot_density + Avg_LSA, random = ~ 1 | Block, data = dat)
-)
+#
+#for reference
+#individual GAMs already tested - for input into pSEM
+m1 <- gam(Shoot_density ~ s(Week, by = Block, k = 4) +
+            Treatment + 
+            s(Week, by = Treatment, k = 4) + 
+            s(Block, bs = "re") +
+            offset(log(Day0_density)),
+          family = "poisson",
+          data = dat)
 
-summary(mnew2_pSEM_randomList, .progressBar = F)
+m2 <- gam(Avg_LSA ~ s(Week, by = Block, k = 4) +
+            Treatment + 
+            s(Week, by = Treatment, k = 4) + 
+            s(Block, bs = "re") + 
+            offset(Day0_Avg_LSA),
+          data = dat)
 
-plot(mnew2_pSEM_randomList)
-
-
-mnew3_pSEM_randomList = psem(
-  lme(Shoot_density ~ Stressor_app * Week + Avg_LSA, random = ~ 1 | Block, data = dat),
-  lme(Avg_LSA ~ Stressor_app + Week, random = ~ 1 | Block, data = dat),
-  lme(lncrust ~ Week + Shoot_density + Avg_LSA, random = ~ 1 | Block, data = dat)
-)
-
-summary(mnew3_pSEM_randomList, .progressBar = F)
-
-plot(mnew3_pSEM_randomList)
+m3 <- gam(Crustacean_abundance ~ s(Week, by = Block, k = 4) + Treatment + 
+            s(Week, by = Treatment, k = 4) + 
+            s(Block, bs = "re") + offset(log(Day0_crustacean + 0.01)),
+          family = nb(),
+          data = dat)
