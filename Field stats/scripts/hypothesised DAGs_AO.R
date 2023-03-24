050d0ffcbf4f3b82aa5ec73852d9c4ad1e824021
Andria <ostro2al@cmich.edu>
2023-03-24 15:50:02 +1000

    Update hypothesised DAGs_AO.R

--- Field stats/scripts/hypothesised DAGs_AO.R
+++ Field stats/scripts/hypothesised DAGs_AO.R
@@ -8,7 +8,7 @@ library(dagitty)
 library(ggdag)
 
 #read csv
-dat <- read.csv("data/DataStacked_Control.csv")
+dat <- read.csv("../Data/DataStacked_Control.csv")
 str(dat)
 dat$Stressor_app <- as.factor(dat$Stressor_app)
 dat$Treatment <- as.factor(dat$Treatment)
@@ -22,7 +22,14 @@ sort(dat$Week)
 #model 1
 m1 <- dagitty( "dag {Treatment -> Shoot_density
                 Week -> Shoot_density
-                x -> Shoot_density
+                Block -> Shoot_density
+                Treatment -> Avg_LSA
+                Week -> Avg_LSA
+                Block -> Avg_LSA
+                Avg_LSA -> Crustacean_abundance
+                Shoot_density -> Crustacean_abundance
+                Block -> Crustacean_abundance
+                Avg_LSA <-> Shoot_density
                 }" )
 plot(m1)
 
@@ -37,26 +44,29 @@ paths(m1, "Week", "Avg_LSA", directed = FALSE)
 paths(m1, "Avg_LSA", "Crustacean_abundance", directed = FALSE)
 paths(m1, "Shoot_density", "Crustacean_abundance", directed = FALSE)
 
-node_collider(m1)
-ggdag_collider(m1)
-
 #identify adjustment sets
-adjustmentSets(m1, "Treatment", "Shoot_density", type="minimal")
-adjustmentSets(m1, "Treatment", "Avg_LSA", type="minimal")
-adjustmentSets(m1, "Week", "Shoot_density", type="minimal")
-adjustmentSets(m1, "Week", "Avg_LSA", type="minimal")
-adjustmentSets(m1, "Avg_LSA", "Crustacean_abundance", type="minimal")
-adjustmentSets(m1, "Shoot_density", "Crustacean_abundance", type="minimal")
+adjustmentSets(m1, "Treatment", "Shoot_density", type="canonical")
+adjustmentSets(m1, "Treatment", "Avg_LSA", type="canonical")
+adjustmentSets(m1, "Week", "Shoot_density", type="canonical")
+adjustmentSets(m1, "Week", "Avg_LSA", type="canonical")
+adjustmentSets(m1, "Block", "Shoot_density", type="canonical")
+adjustmentSets(m1, "Block", "Avg_LSA", type="canonical")
+adjustmentSets(m1, "Block", "Crustacean_abundance", type="canonical")
+adjustmentSets(m1, "Avg_LSA", "Crustacean_abundance", type="canonical")
+adjustmentSets(m1, "Shoot_density", "Crustacean_abundance", type="canonical")
 
 
 #model 2
 m2 <- dagitty( "dag {Treatment -> Shoot_density
                 Week -> Shoot_density
+                Block -> Shoot_density
                 Treatment -> Avg_LSA
                 Week -> Avg_LSA
+                Block -> Avg_LSA
                 Week -> Crustacean_abundance
                 Avg_LSA -> Crustacean_abundance
                 Shoot_density -> Crustacean_abundance
+                Block -> Crustacean_abundance
                 Avg_LSA <-> Shoot_density
                 }" )
 plot(m2)
@@ -73,32 +83,32 @@ paths(m2, "Week", "Crustacean_abundance", directed = FALSE)
 paths(m2, "Avg_LSA", "Crustacean_abundance", directed = FALSE)
 paths(m2, "Shoot_density", "Crustacean_abundance", directed = FALSE)
 
-node_collider(m2)
-ggdag_collider(m2)
-
 #identify adjustment sets
-adjustmentSets(m2, "Treatment", "Shoot_density", type="minimal")
-adjustmentSets(m2, "Treatment", "Avg_LSA", type="minimal")
-adjustmentSets(m2, "Week", "Shoot_density", type="minimal")
-adjustmentSets(m2, "Week", "Avg_LSA", type="minimal")
-adjustmentSets(m2, "Week", "Crustacean_abundance", type="minimal")
-adjustmentSets(m2, "Avg_LSA", "Crustacean_abundance", type="minimal")
-adjustmentSets(m2, "Shoot_density", "Crustacean_abundance", type="minimal")
+adjustmentSets(m2, "Treatment", "Shoot_density", type="canonical")
+adjustmentSets(m2, "Treatment", "Avg_LSA", type="canonical")
+adjustmentSets(m2, "Week", "Shoot_density", type="canonical")
+adjustmentSets(m2, "Week", "Avg_LSA", type="canonical")
+adjustmentSets(m2, "Week", "Crustacean_abundance", type="canonical")
+adjustmentSets(m2, "Block", "Shoot_density", type="canonical")
+adjustmentSets(m2, "Block", "Avg_LSA", type="canonical")
+adjustmentSets(m2, "Block", "Crustacean_abundance", type="canonical")
+adjustmentSets(m2, "Avg_LSA", "Crustacean_abundance", type="canonical")
+adjustmentSets(m2, "Shoot_density", "Crustacean_abundance", type="canonical")
 
 
 #model 3
 m3 <- dagitty( "dag {Treatment -> Shoot_density
                 Week -> Shoot_density
+                Block -> Shoot_density
                 Treatment -> Avg_LSA
                 Week -> Avg_LSA
+                Block -> Avg_LSA
                 Week -> Crustacean_abundance
                 Treatment -> Crustacean_abundance
+                Block -> Crustacean_abundance
                 Avg_LSA -> Crustacean_abundance
                 Shoot_density -> Crustacean_abundance
                 Avg_LSA <-> Shoot_density
-                block -> Shoot_density
-                block -> Avg_LSA
-                block -> Crustacean_abundance
                 }" )
 plot(m3)
 
@@ -115,19 +125,15 @@ paths(m3, "Treatment", "Crustacean_abundance", directed = FALSE)
 paths(m3, "Avg_LSA", "Crustacean_abundance", directed = FALSE)
 paths(m3, "Shoot_density", "Crustacean_abundance", directed = FALSE)
 
-node_collider(m3)
-ggdag_collider(m3)
-
-
 #identify adjustment sets
-adjustmentSets(m3, "Treatment", "Shoot_density", type="minimal")
-adjustmentSets(m3, "Treatment", "Avg_LSA", type="minimal")
+adjustmentSets(m3, "Treatment", "Shoot_density", type="canonical")
 adjustmentSets(m3, "Treatment", "Avg_LSA", type="canonical")
-adjustmentSets(m3, "Week", "Shoot_density", type="minimal")
-adjustmentSets(m3, "Week", "Avg_LSA", type="minimal")
-adjustmentSets(m3, "Week", "Crustacean_abundance", type="minimal")
-adjustmentSets(m3, "Treatment", "Crustacean_abundance", type="minimal")
+adjustmentSets(m3, "Week", "Shoot_density", type="canonical")
+adjustmentSets(m3, "Week", "Avg_LSA", type="canonical")
+adjustmentSets(m3, "Week", "Crustacean_abundance", type="canonical")
 adjustmentSets(m3, "Treatment", "Crustacean_abundance", type="canonical")
-adjustmentSets(m3, "Avg_LSA", "Crustacean_abundance", type="minimal")
+adjustmentSets(m3, "Block", "Shoot_density", type="canonical")
+adjustmentSets(m3, "Block", "Avg_LSA", type="canonical")
+adjustmentSets(m3, "Block", "Crustacean_abundance", type="canonical")
 adjustmentSets(m3, "Avg_LSA", "Crustacean_abundance", type="canonical")
-adjustmentSets(m3, "Shoot_density", "Crustacean_abundance", type="minimal")
+adjustmentSets(m3, "Shoot_density", "Crustacean_abundance", type="canonical")
