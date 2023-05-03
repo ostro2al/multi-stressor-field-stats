4e312b9650b1e38a8c4ff4351028360e8060d56b
Andria <ostro2al@cmich.edu>
2023-05-03 12:58:11 +1000

    Update Check LSA-Crust data.R

--- Field stats/Check LSA-Crust data.R
+++ Field stats/Check LSA-Crust data.R
@@ -4,43 +4,17 @@
 library(rstatix)
 library(dplyr)
 library(ggplot2)
+library(cowplot)
 
 #read csv
 dat <- read.csv("data/Data_Control.csv")
 
-#plot raw data for week 6
+#plot crustacean abundance vs LSA to verify weak mediation effect
 dat %>%
-  filter(Week == 6) %>%
   ggplot()+
-  (aes(x = Avg_LSA, y = Crustacean_abundance))+
-  facet_grid(~Treatment)+
-  geom_point() +
-  theme_cowplot()
-
-#get mean data for LSA and crust
-options(tibble.print_min = Inf)
-dat2 <- dat %>% 
-  group_by(Treatment, Week) %>%
-  get_summary_stats(Avg_LSA, type = "mean_se")
-names(dat2)[5] <- "LSA"
-dat2 
-
-
-dat3 <- dat %>% 
-  group_by(Treatment, Week) %>%
-  get_summary_stats(Crustacean_abundance, type = "mean_se")
-names(dat3)[5] <- "Crust"
-dat3 <- dat3[,!names(dat3) %in% c("Week", "Treatment", "variable", "n", "se")]
-dat3  
-
-dat4 <- cbind(dat2, dat3)
-dat4
-
-#mean data
-dat4 %>%
-  filter(Week == 6) %>%
-  ggplot()+
-  (aes(x = LSA, y = Crust, group = Treatment, colour = Treatment))+
-  #facet_grid(~Treatment)+
-  geom_point() +
-  theme_cowplot()
+  (aes(x = Avg_LSA, y = Crustacean_abundance, colour = Treatment))+
+  facet_grid(Treatment~Week)+
+  geom_point(size = 2.5, position = position_dodge(0.8)) +
+  theme_cowplot()+
+  xlab("Leaf surface area (cm^2)")+
+  ylab("Crustacean abundance")
